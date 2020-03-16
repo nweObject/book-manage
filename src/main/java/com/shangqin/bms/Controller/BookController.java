9472065dab9dd76ce9a51ae8571bb0426162b335
admin <1558538904@qq.com>
2020-03-16 16:16:17 +0800

    使用了自定义返回值

--- src/main/java/com/shangqin/bms/Controller/BookController.java
+++ src/main/java/com/shangqin/bms/Controller/BookController.java
@@ -8,7 +8,6 @@ import com.shangqin.bms.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
